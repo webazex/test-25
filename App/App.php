5b49985c0e5c38d2e9f6fc791f6380590c75c3bc
AlexG <alex@alexgoldberg.online>
2023-11-13 20:53:15 +0200

    created test metod, checking autoload work

--- App/App.php
+++ App/App.php
@@ -6,5 +6,11 @@ namespace Webazex\App;
 
 class App
 {
+    public function __construct()
+    {
 
+    }
+    public function test(){
+        echo "this";
+    }
 }
--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php
 require_once 'autoload.php';
+use Webazex\App\App as App;
+$app = new App();
+$app->test();
