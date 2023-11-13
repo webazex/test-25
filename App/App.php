4afa37015b3b849c87acc3b814d0f6a4831106c9
AlexG <alex@alexgoldberg.online>
2023-11-13 20:52:08 +0200

    created base autoload;

---
+++ App/App.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Webazex\App;
+
+
+class App
+{
+
+}
--- autoload.php
+++ autoload.php
@@ -1,1 +1,7 @@
 <?php
+spl_autoload_register(function ($str){
+    if(strpos($str, "Webazex") !== false){
+        $trueStr = str_replace("Webazex\\", '', $str);
+        require_once $trueStr.'.php';
+    }
+});
